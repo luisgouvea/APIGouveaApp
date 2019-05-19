bc7e7843a725a8e3b6c0e6b1e494261df584c16d
luisgouvea <luiseduardo.gouvea@hotmail.com>
2019-05-19 02:05:55 -0300

    fd ds fsd

---
+++ APIGouveaApp/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using Persistencia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIGouveaApp.Controllers
+{
+    public class OrderController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage GetOrderByNumber()
+        {
+
+            List<ItemOrder> itensOrder = Persistencia.DD.OrderDD.GetOrderByNumber("4009");
+            for (int i = 0; i < itensOrder.Count - 1; i++)
+            {
+                ItemOrder itemTarget = itensOrder[i];
+                Persistencia.DD.OrderDD.GetItemBarcode(ref itemTarget);
+                Persistencia.DD.OrderDD.GetItemDescription(ref itemTarget);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, itensOrder);
+        }
+    }
+}
